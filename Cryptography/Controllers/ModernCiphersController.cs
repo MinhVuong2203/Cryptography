0aa25e5ad49553669c72c87a1849d42bae896c6f
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2026-04-19 10:34:18 +0700

    tạo controler nhé

---
+++ Cryptography/Controllers/ModernCiphersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cryptography.Controllers
+{
+    public class ModernCiphersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
